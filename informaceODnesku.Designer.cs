5ec81b8986f3dd7a6b9f8c12be2c4cded66872ac
mchyla7 <mchyla7@gmail.com>
2024-11-14 11:22:19 +0100

    ➕ Přidán program na práci s datumem

---
+++ informaceODnesku.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class informaceODnesku
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lAktualniCas = new System.Windows.Forms.Label();
+            this.bUpdate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(36, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // lAktualniCas
+            // 
+            this.lAktualniCas.AutoSize = true;
+            this.lAktualniCas.Location = new System.Drawing.Point(36, 291);
+            this.lAktualniCas.Name = "lAktualniCas";
+            this.lAktualniCas.Size = new System.Drawing.Size(67, 13);
+            this.lAktualniCas.TabIndex = 5;
+            this.lAktualniCas.Text = "Aktuální čas";
+            // 
+            // bUpdate
+            // 
+            this.bUpdate.Location = new System.Drawing.Point(36, 143);
+            this.bUpdate.Name = "bUpdate";
+            this.bUpdate.Size = new System.Drawing.Size(97, 35);
+            this.bUpdate.TabIndex = 6;
+            this.bUpdate.Text = "Update";
+            this.bUpdate.UseVisualStyleBackColor = true;
+            this.bUpdate.Click += new System.EventHandler(this.bUpdate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // informaceODnesku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 429);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.bUpdate);
+            this.Controls.Add(this.lAktualniCas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "informaceODnesku";
+            this.Text = "informaceODnesku";
+            this.Load += new System.EventHandler(this.informaceODnesku_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lAktualniCas;
+        private System.Windows.Forms.Button bUpdate;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ informaceODnesku.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class informaceODnesku : Form
+    {
+        public informaceODnesku()
+        {
+            InitializeComponent();
+        }
+
+        private void informaceODnesku_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.MaxDate = Convert.ToDateTime("2025.1.31");
+            dateTimePicker1.MinDate = DateTime.Now;
+        }
+
+        private void bUpdate_Click(object sender, EventArgs e)
+        {
+
+            DateTime nyni = dateTimePicker1.Value;
+
+            label1.Text = nyni.ToString();
+            label2.Text = nyni.Day.ToString();
+            label3.Text = nyni.Month.ToString();
+            label4.Text = nyni.Year.ToString();
+
+
+            label5.Text = nyni.Hour + ":" + nyni.Minute + ":" + nyni.Second; 
+
+        }
+    }
+}
