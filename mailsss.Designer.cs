e74450c03bee13c63fa3b5ba8eed574e729bca80
mchyla7 <mchyla7@gmail.com>
2025-01-16 11:27:37 +0100

    ➕ Přidány mailsss

--- mailsss.Designer.cs
+++ mailsss.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(391, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(332, 200);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // bOpen
+            // 
+            this.bOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.bOpen.Location = new System.Drawing.Point(75, 56);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(108, 36);
+            this.bOpen.TabIndex = 1;
+            this.bOpen.Text = "Otevřít";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // mailsss
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 500);
+            this.Controls.Add(this.bOpen);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "mailsss";
             this.Text = "mailsss";
+            this.Load += new System.EventHandler(this.mailsss_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button bOpen;
     }
 }
--- mailsss.cs
+++ mailsss.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -14,6 +15,43 @@ namespace WindowsFormsApplication1
         public mailsss()
         {
             InitializeComponent();
+        }
+
+        private void mailsss_Load(object sender, EventArgs e)
+        {
+            // Zadání
+
+            // Vytvořte formulářovou aplikaci.
+            // Bude pracovat se souborem mail.txt.
+            // Zkontroluje, zda obsahuje záznamy ve struktuře text.text@doména.
+            // Soubor výsledky kontroly zobrazí uživateli a zeptá se, zda vyhovující záznamy má zpravovat do výstupu.
+            // Zeptá se na jméno souboru.
+            // Program vytvoří nový soubor.
+            // Záznamy budou seřazené podle příjmení.
+            // Každý záznam bude na samostatném řádku
+            // Záznam: příjmení jméno mail
+            // adam.fojtik@mendelova-stredni.cz
+
+        }
+
+        private void bOpen_Click(object sender, EventArgs e)
+        {
+            string[] data;
+            openFileDialog1.ShowDialog();
+            string soubor = openFileDialog1.FileName;
+
+            try
+            {
+                data = File.ReadAllLines(soubor);
+            }
+            catch
+            {
+                MessageBox.Show("Soubor nebyl nalezen");
+                return;
+            }
+
+            string[] jednotliveEmaily = data.SelectMany(line => line.Split(';')).Where(email => !string.IsNullOrEmpty(email)).ToArray();
+            richTextBox1.Text = jednotliveEmaily.ToString();
         }
     }
 }
