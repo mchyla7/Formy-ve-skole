91314cd6dfd9c88aa05e6afd0d1b676ab40dda94
mchyla7 <mchyla7@gmail.com>
2025-03-06 10:45:20 +0100

    ➕ Přidán OOP s formuláři

---
+++ Console application/ObjektivneOrientovaneProgramovani/ObjektivneOrientovaneProgramovani/ooformular/ooformular/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ooformular
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hodnoty.hodnota += textBox1.Text + Environment.NewLine;
+
+            Form2 form2 = new Form2(); // Vytvoření nového formuláře
+
+            this.Hide();
+
+            form2.ShowDialog();
+            // this.Close();
+        }
+    }
+}
---
+++ Console application/ObjektivneOrientovaneProgramovani/ObjektivneOrientovaneProgramovani/ooformular/ooformular/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ooformular
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            richTextBox1.Text = Hodnoty.hodnota;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.ShowDialog();
+        }
+    }
+}
